f0ef92b8f05a5b069e7bed0aaabce34ca545021c
fabiorea180677 <fabrea@gmail.com>
2021-03-25 01:13:47 +0100

    SCSS style implementation

---
+++ launcher.bat
@@ -0,0 +1,2 @@
+REM docker run --rm --name slate -p 4567:4567 -v ${pwd}/source:/srv/slate/source slatedocs/slate serve
+docker run --rm --name slate -p 4567:4567 -v %cd%/srv/slate/source slatedocs/slate serve
